c66f0fd60088ef06bd4a4ac315d3a4ca26665caa
Yuya Watanabe <yusato312630@gmail.com>
2023-12-21 11:19:22 +0900

    modified

--- report05/problem01.f90
+++ report05/problem01.f90
@@ -3,7 +3,7 @@ program bessel1
    integer :: nsteps   !全ステップ数
    integer :: nout     !データ出力回数
    integer :: ntime    !出力データの間隔
-   integer :: i, r
+   integer :: i
    real :: h   !xの刻み幅
    real :: xend    !xの最終値
    real :: xstep   !xの出力刻み幅
@@ -12,7 +12,7 @@ program bessel1
    real, dimension(0:10000) :: x1, y1, z1
 
    open (1, file='input.txt')
-   open (2, file='output.dat')
+   open (21, file='output.dat')
 
    read (1, '(6f6.3)') h, xend, xstep, x0, y0, z0
    close (1)
@@ -29,7 +29,7 @@ program bessel1
    !    x0 = 0において, y0 = 1, dy/dx = 0, (1/x)(dy/dx) = -1/2
    x1(1) = x1(0) + h
    y1(1) = y1(0) + h*0      !f0 = dy/dz = 0
-   z1(1) = z1(0) - h*0.50   !g0 = -(1/x)(dy/dx) - y = -(-1/2) - 1 = -1/2
+   z1(1) = z1(0) - h/2   !g0 = -(1/x)(dy/dx) - y = -(-1/2) - 1 = -1/2
 
    do i = 1, nsteps - 1
       x1(i + 1) = x1(i) + h
@@ -42,9 +42,9 @@ program bessel1
    end do
 
    do i = 0, nsteps, ntime
-      write (2, '(4f10.6)') x1(i), y1(i), z1(i), abs(y1(i) - bessel(i))
+      write (21, '(4f10.6)') x1(i), y1(i), z1(i), abs(y1(i) - bessel(i))
    end do
-   close (2)
+   close (21)
 
 contains
    real function f(x, y, z) result(fresult)
